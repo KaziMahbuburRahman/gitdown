429be18c209adbd8c440cabf48450f42bbe641bf
NoobMahbub <Mahbubbengali@gmail.com>
2024-01-31 00:34:39 +0600

    added a nice card to show the thumbnail

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,9 @@ function App() {
   const [downloadLink, setDownloadLink] = useState('');
   const [downloadFileName, setDownloadFileName] = useState('');
   const [sizeMB, setsizeMB] = useState('');
-  const [showModal, setShowModal] = useState(false);
+  const [data, setData] = useState('');
+  const [loading, setLoading] = useState(false);
+
 
   // useEffect(() => {
   //   // Call thumbImg when the component mounts
@@ -68,7 +70,7 @@ function App() {
       fetch(githubAPI)
         .then(response => response.json())
         .then(data => {
-          console.log(data);
+          setData(data);
           // Filter out only files from the data
           //will uncomment later
           // const files = data.filter(item => item.type === 'file');
@@ -86,6 +88,7 @@ function App() {
   }
 
   const handlePaste = (event) => {
+    setLoading(true);
     // Handle paste event
     console.log('Pasted:', event.clipboardData.getData('text'));
     const pastedText = event.clipboardData.getData('text');
@@ -99,6 +102,7 @@ function App() {
 
   const handleButtonClick = async (e) => {
     e.preventDefault()
+    setLoading(true);
     const url = e.target.elements.urlInput.value;
     downRepo(url)
 
@@ -205,7 +209,7 @@ function App() {
 
 
         </form>
-
+        {loading && !sizeMB && <LoadingIcon />}
         {/* <label htmlFor="my-modal-3" className="btn">open modal</label> */}
 
         {/* Put this part before </body> tag */}
@@ -221,35 +225,120 @@ function App() {
           </div>
         </div>
 
-        <div className='flex justify-center items-center'>
+        {
+          sizeMB && (<div className='flex justify-center items-center'>
+            <div className='w-[75%]'>
+              <h2 className='text-2xl font-semibold'>Zipped {data.length} Files</h2>
 
-          <button
-            onClick={() => {
+              {/* //files name one by one */}
+              {data.map((item) => (
+                <p>{item.name}</p>
+              ))}
 
-              const a = document.createElement('a');
-              a.href = downloadLink;
-              a.download = downloadFileName;
-              console.log(downloadLink)
-              console.log(downloadFileName)
-              document.body.appendChild(a);
-              a.click();
-              //delay
-              setTimeout(() => {
-                document.getElementById('my-modal-3').checked = true;
-              }, 400);
-              // document.getElementById('my-modal-3').checked = true;
-              // URL.revokeObjectURL(downloadLink);
-            }}
-            style={{ boxShadow: '0 5px 15px 5px rgba(34, 125, 199, .42)' }}
-            className="text-xl px-10 py-5 text-center align-middle m-0-auto bg-sky-600 rounded-xl hover:bg-slate-700 border-0 border-none text-white duration-300 hover:shadow-none focus:outline-none focus:ring-0 focus:border-none active:outline-none active:ring-0 active:border-none shadow-xl transition duration-300 ease-in-out"
-          >
-            Download
-          </button>
-          <div>
-            <img src={`https://opengraph.githubassets.com/e61b97681f68c6b6893f9386c313d502fdfb7b512bdf4f187b2582bc0378b0c6/${owner}/${repo}`} alt="" />
-            <h2>size: {sizeMB}</h2>
-          </div>
-        </div>
+              <h2 className='text-xl font-bold  text-gray-700'>Size: {sizeMB}</h2>
+
+              <button
+                onClick={() => {
+
+                  const a = document.createElement('a');
+                  a.href = downloadLink;
+                  a.download = downloadFileName;
+                  console.log(downloadLink)
+                  console.log(downloadFileName)
+                  document.body.appendChild(a);
+                  a.click();
+                  //delay
+                  setTimeout(() => {
+                    document.getElementById('my-modal-3').checked = true;
+                  }, 400);
+                  // document.getElementById('my-modal-3').checked = true;
+                  // URL.revokeObjectURL(downloadLink);
+                }}
+                style={{ boxShadow: '0 5px 15px 5px rgba(34, 125, 199, .42)' }}
+                className="text-xl px-10 py-5 text-center align-middle m-0-auto bg-sky-600 rounded-xl hover:bg-slate-700 border-0 border-none text-white duration-300 hover:shadow-none focus:outline-none focus:ring-0 focus:border-none active:outline-none active:ring-0 active:border-none shadow-xl transition duration-300 ease-in-out"
+              >
+                Download
+              </button>
+            </div>
+
+            <div>
+
+              <>
+                {/*<!-- Component: Social story card --> */}
+                <div className="overflow-hidden rounded bg-white text-slate-500 shadow-md shadow-slate-200">
+                  {/*  <!-- Header--> */}
+
+                  {/*  <!-- Image --> */}
+                  <figure>
+                    <img
+                      src={`https://opengraph.githubassets.com/e61b97681f68c6b6893f9386c313d502fdfb7b512bdf4f187b2582bc0378b0c6/${owner}/${repo}`}
+                      alt="card image"
+                      className="w-full"
+                    />
+                  </figure>
+                  {/*  <!-- Body--> */}
+                  <div className="p-6">
+                    <p>
+                      Spend days here, exploring a way of life by bicycle                      
+                    </p>
+                  </div>
+                  {/*  <!-- Action icon buttons --> */}
+                  <div className="flex justify-end gap-2 p-2 pt-0">
+                    <button className="inline-flex h-10 items-center justify-center gap-2 justify-self-center whitespace-nowrap rounded px-5 text-sm font-medium tracking-wide text-emerald-500 transition duration-300 hover:bg-emerald-100 hover:text-emerald-600 focus:bg-emerald-200 focus:text-emerald-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:text-emerald-300 disabled:shadow-none disabled:hover:bg-transparent">
+                      <span className="relative only:-mx-6">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="1.5"
+                          role="graphics-symbol"
+                          aria-labelledby="title-81 desc-81"
+                        >
+                          <title id="title-81">Icon title</title>
+                          <desc id="desc-81">
+                            A more detailed description of the icon
+                          </desc>
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                          />
+                        </svg>
+                      </span>
+                    </button>
+                    <button className="inline-flex h-10 items-center justify-center gap-2 justify-self-center whitespace-nowrap rounded px-5 text-sm font-medium tracking-wide text-emerald-500 transition duration-300 hover:bg-emerald-100 hover:text-emerald-600 focus:bg-emerald-200 focus:text-emerald-700 focus-visible:outline-none disabled:cursor-not-allowed disabled:text-emerald-300 disabled:shadow-none disabled:hover:bg-transparent">
+                      <span className="relative only:-mx-6">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="h-5 w-5"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                          stroke="currentColor"
+                          strokeWidth="1.5"
+                          role="graphics-symbol"
+                          aria-labelledby="title-82 desc-82"
+                        >
+                          <title id="title-82">Icon title</title>
+                          <desc id="desc-82">
+                            A more detailed description of the icon
+                          </desc>
+                          <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z"
+                          />
+                        </svg>
+                      </span>
+                    </button>
+                  </div>
+                </div>
+                {/*<!-- End Social story card --> */}
+              </>
+            </div>
+          </div>)
+        }
         {/* loading */}
         {/* <LoadingIcon /> */}
       </div>
