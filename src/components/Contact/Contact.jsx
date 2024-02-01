d18c46d5b340c08f92841766437b03e5c07805eb
NoobMahbub <Mahbubbengali@gmail.com>
2024-02-01 19:40:29 +0600

    successfully displayed send mail message using windui alert on contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -8,14 +8,8 @@ import ThemeChanger from '../../shared/ThemeChanger';
 // import { ToastContainer, toast } from 'react-toastify';
 
 const Contact = () => {
-    const [isModalOpen, setModalOpen] = useState(false);
-    const openModal = () => {
-        setModalOpen(true);
-    }
-    const closeModal = () => {
-        setModalOpen(false);
-    }
-    const [daisyTheme, setDaisyTheme] = useState('');
+    const [successMessage, setSuccessMessage] = useState(false);
+
 
     const form = useRef(null);
     const sendEmail = (e) => {
@@ -38,12 +32,13 @@ const Contact = () => {
         // });
 
         form.current.reset();
+        setSuccessMessage(true);
     };
 
     return (
         <>
-            <NavbarComponent openModal={openModal} />
-            <ThemeChanger isModalOpen={isModalOpen} closeModal={closeModal} daisyTheme={daisyTheme} setDaisyTheme={setDaisyTheme} />
+            <NavbarComponent />
+
             {/* 
             <ToastContainer
                 position="top-right"
@@ -64,9 +59,9 @@ const Contact = () => {
                         <h3 className=''>Have any questions ? <br className='lg:hidden' />
                             We love to hear from you .</h3>
                     </div>
-                    <div className="flex flex-col justify-center  lg:flex-row container mx-auto px-8 gap-6">
+                    <div className="flex flex-col justify-center items-center container mx-auto px-8 gap-6">
 
-                        <form ref={form} onSubmit={sendEmail} className="lg:w-1/2 space-y-2 p-5 bg-base-100 rounded-lg">
+                        <form ref={form} onSubmit={sendEmail} className="max-w-[700px] space-y-2 p-5 bg-base-100 rounded-lg">
 
                             <input
                                 required
@@ -104,6 +99,8 @@ const Contact = () => {
                                 className='w-full text-black border rounded-lg py-1 text-xl pl-3 hover:border-primary duration-300'
                             />
 
+
+
                             <button
                                 className='px-5 py-3  border bg-primary duration-300 hover:bg-[#6f49c7] rounded-lg text-lg w-full text-white'
                                 type='submit'
@@ -114,8 +111,44 @@ const Contact = () => {
 
                         </form>
 
-
+                        {/* Message Sent confirmation */}
+                        {
+                            successMessage && <div
+                                className="flex max-w-[657px] bg-base-100 items-start gap-4 rounded border border-emerald-100 bg-emerald-50 px-2 py-3 text-sm text-emerald-500"
+                                role="alert"
+                            >
+                                {/*  <!-- Icon --> */}
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5 shrink-0"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    strokeWidth="1.5"
+                                    role="graphics-symbol"
+                                    aria-labelledby="title-06 desc-06"
+                                >
+                                    <title id="title-06">Icon title</title>
+                                    <desc id="desc-06">A more detailed description of the icon</desc>
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                                    />
+                                </svg>
+                                {/*  <!-- Text --> */}
+                                <div>
+                                    <h3 className="mb-2 font-semibold">
+                                        Message Sent Successfully!
+                                    </h3>
+                                    <p>
+                                        Thanks for your feedback{" "}
+                                    </p>
+                                </div>
+                            </div>
+                        }
                     </div>
+
                 </div>
             </div>
             <Footer />
