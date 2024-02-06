81250a3982ce3cce8716a857c3a68357816bcb7f
NoobMahbub <Mahbubbengali@gmail.com>
2024-02-06 16:25:47 +0600

    changed preloader bg to white

--- src/Preloader.jsx
+++ src/Preloader.jsx
@@ -3,7 +3,7 @@ import Logo from "./img/GitDown-brain.png";
 
 export default function Preloader() {
     return (
-        <div className='flex flex-col justify-center items-center h-screen'>
+        <div className='bg-white flex flex-col justify-center items-center h-screen'>
             <img src={Logo} alt="logo" className="w-24 h-24" />
             <div className="flex items-center justify-center mt-4">
                 <div className="w-3 h-3 bg-gray-600 rounded-full animate-bounce mr-1"></div>
